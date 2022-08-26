f44ce58dc6e593b111d68b2d8bd7625277bb9ce1
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2022-08-27 05:08:01 +0530

    committed for /

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 // use Routes
 app.use('/api/events', events);
 
